eb17989cfe3b953d57c4c0b33afef8eddad8ddb9
kuldeep-sib <kuldeep.kumar@sendinblue.com>
2021-07-12 23:23:01 +0530

    multiple lists added

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
 const ioredis = require("ioredis");
 const uuid = require("uuid");
-const { config } = require("./config")
+const { config } = require("./config");
 
 const COUNT = process.env.COUNT ? parseInt(process.env.COUNT) : 100000;
+
+const LIST_COUNT = process.env.LIST_COUNT
+  ? parseInt(process.env.LIST_COUNT)
+  : 1;
 const initialCount = 1000;
-const list = `1:test.com:transact`;
-const processingList = `1:test.com:processing`;
 
-
+const Lists = [];
 // password
-const REDIS_PASSWORD = process.env.REDIS_PASSWORD
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
 if (REDIS_PASSWORD) {
-    config.redisQueuesConf.password = REDIS_PASSWORD;
+  config.redisQueuesConf.password = REDIS_PASSWORD;
 }
-const redis = new ioredis(config.redisQueuesConf);
-
+const redis = new ioredis();
 
 const rpoplpushlatency = [];
 const lpushLatency = [];
-const lremLatency = []
+const lremLatency = [];
 // start
 Do().then(() => {
   process.exit(0);
 });
+
+function generateListNames() {
+  for (let i = 0; i < LIST_COUNT; i++) {
+    const isp = Math.floor(Math.random() * (100 - 1) + 1);
+    const domain = uuid.v4().substring(0, 8);
+    const listName = `${isp}:${domain}:transact`;
+    Lists.push(listName);
+  }
+}
 async function Do() {
   try {
+    generateListNames();
     await init();
-    const res = await Promise.all([pushToQueue(), moveMessage()]);
+    const res = await Promise.all([pushData(), popData()]);
     console.log(`RPOPLPUSH avg in ms:`, getAvg(rpoplpushlatency));
     console.log(`LPUSH avg in ms:`, getAvg(lpushLatency));
-    console.log("Lrem latency in ms:", getAvg(lremLatency))
+    console.log("Lrem latency in ms:", getAvg(lremLatency));
   } catch (error) {
     console.error(error);
   }
@@ -45,11 +56,17 @@ function getAvg(elmt) {
 }
 
 async function init() {
-  for (let i = 0; i < initialCount; i++) {
-    await redis.lpush(list, uuid.v4());
+  for (const list of Lists) {
+    for (let i = 0; i < initialCount; i++) {
+      await redis.lpush(list, uuid.v4());
+    }
   }
 }
-async function pushToQueue() {
+
+async function pushData() {
+  await Promise.all(Lists.map((list) => pushToQueue(list)));
+}
+async function pushToQueue(list) {
   for (let i = 0; i < COUNT - initialCount; i++) {
     const start_time = process.hrtime();
     await redis.lpush(list, uuid.v4());
@@ -58,8 +75,11 @@ async function pushToQueue() {
     lpushLatency.push(rl);
   }
 }
-
-async function moveMessage() {
+async function popData() {
+  await Promise.all(Lists.map((list) => moveMessage(list)));
+}
+async function moveMessage(list) {
+  const processingList = list.replace("transact", "processing");
   while (1) {
     const start_time = process.hrtime();
     const msg = await redis.rpoplpush(list, processingList);
@@ -69,16 +89,14 @@ async function moveMessage() {
     if (!msg) {
       break;
     }
-    processMessage(msg);
+    processMessage(msg, processingList);
   }
 }
 
-function processMessage(msg) {
-  setTimeout(async () => {
+async function processMessage(msg, processingList) {
     const start_time = process.hrtime();
     await redis.lrem(processingList, 1, msg);
     const endTime = process.hrtime(start_time);
     const rl = (endTime[0] * 1000000000 + endTime[1]) / 1000000;
     lremLatency.push(rl);
-  }, 200);
 }
